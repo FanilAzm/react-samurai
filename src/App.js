c33d60f202dc091d1fda9ff19d401086532ecbfb
FanilAzm <fanil.azmagulov@mail.ru>
2022-02-09 16:20:22 +0500

    fix app

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import Preloader from './components/common/Preloader/Preloader';
 import store from './redux/redux-store';
 import {Provider} from 'react-redux';
 import withSuspense from './hoc/withSuspense';
+import { Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
@@ -36,23 +38,34 @@ class App extends React.Component {
           <HeaderContainer />
           <NavBar />
           <div className="app-wrapper-content">
-            <Route
-              path="/profile/:userId?"
-              render={ withSuspense(ProfileContainer) }
-            />
-            <Route
-              exact
-              path="/dialogs"
-              render={ withSuspense(DialogsContainer) }
-            />
-            <Route
-              path="/users"
-              render={ () => <UsersContainer/>}  
-            />
-            <Route path="/news" component={News}/>
-            <Route path="/music" component={Music}/>
-            <Route path="/settings" component={Settings}/>
-            <Route path="/login" component={Login}/>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={ withSuspense(ProfileContainer) }
+              />
+              <Route
+                path="/profile/:userId?"
+                render={ withSuspense(ProfileContainer) }
+              />
+              <Route
+                exact
+                path="/dialogs"
+                render={ withSuspense(DialogsContainer) }
+              />
+              <Route
+                path="/users"
+                render={ () => <UsersContainer/>}  
+              />
+              <Route path="/news" component={News}/>
+              <Route path="/music" component={Music}/>
+              <Route path="/settings" component={Settings}/>
+              <Route path="/login" component={Login}/>
+              <Route
+                path="*"
+                render={ () => <div>404 NotFound</div>}  
+              />
+            </Switch>
           </div>
           <OnlineUsers />
         </div>
